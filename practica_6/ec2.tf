fecbe6c72611254af2a08159325ac9ed5248fa4d
Victor Rodriguez Garcia <vrodriguez@opentrends.net>
2024-08-13 17:37:24 +0200

    data from aws y documentacion

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -2,4 +2,8 @@ resource "aws_instance" "public_instance" {
   ami           = "ami-02af70169146bbdd3"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet.id
+  key_name      = data.aws_key_pair.key.key_name
+  tags = {
+    Name = "Public instance"
+  }
 }
