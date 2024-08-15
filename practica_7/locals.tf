9a2b703200d4b8bcc96123c554e4119cca165210
Victor Rodriguez Garcia <vrodriguez@opentrends.net>
2024-08-15 16:46:56 +0200

    locals 2

--- practica_7/locals.tf
+++ practica_7/locals.tf
@@ -1,3 +1,10 @@
 locals {
-  sufix = "${var.tags.project}-${var.tags.region}-${var.tags.env}"
+  sufix    = "${var.tags.project}-${var.tags.region}-${var.tags.env}"
+  s3-sufix = random_string.s3-sufix.id
+}
+
+resource "random_string" "s3-sufix" {
+  length  = 8
+  special = false
+  upper   = false
 }
